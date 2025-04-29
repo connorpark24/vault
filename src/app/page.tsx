d99a43265c45944e8087900408f8c490c0dccebb
Connor Park <connorpark024@gmail.com>
2025-04-29 19:59:00 -0400

    tinker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
-import { Input } from "@/components/ui/input";
-import Sidebar from "@/components/Sidebar";
-
 import { resources, Resource } from "@/lib/data";
 
 export default function Home() {
   return (
     <>
-      <div className="p-6 h-20 w-screen border-b border-neutral-300 flex items-center justify-between fixed top-0 bg-white z-10">
-        <h1 className="tracking-tighter text-3xl font-medium">Vault</h1>
-        <div className="w-96">
-          <Input type="text" placeholder="Search for domain..." />
-        </div>
-      </div>
-
       <div className="flex flex-row w-screen mt-20">
-        <Sidebar />
         <div className="grow grid grid-cols-2 w-full overflow-scroll p-4 gap-2 ml-72">
           {resources.map((resource) => (
             <ResourceCard key={resource.name} resource={resource} />
